42f7af6b117725e0a508c99961b70a5e9baf7500
jbphet <john.blanco@colorado.edu>
2016-03-30 12:39:17 -0600

    refactored to inherit from Object rather than access prototype directly, see #26

--- js/Sound.js
+++ js/Sound.js
@@ -1,27 +1,27 @@
 // Copyright 2013-2015, University of Colorado Boulder
 
 /**
- * Type for loading and playing sounds, works on multiple platforms and
- * supports embedded base64 data.  This uses Web Audio when available,
- * primarily because the webkit platforms were failing with cross-domain
- * errors when attempting to load audio data from embedded data URIs.  This
- * was occurring in mid-September 2013.  Simplification may be possible if the
- * cross-domain issue goes away at some point in the future.
+ * Type for loading and playing sounds, works on multiple platforms and supports embedded base64 data.  This uses Web
+ * Audio when available, primarily because the webkit platforms were failing with cross-domain errors when attempting to
+ * load audio data from embedded data URIs.  This was occurring in mid-September 2013.  Simplification may be possible
+ * if the cross-domain issue goes away at some point in the future.
  */
 define( function( require ) {
   'use strict';
 
   // modules
+  var inherit = require( 'PHET_CORE/inherit' );
   var platform = require( 'PHET_CORE/platform' );
-  var empty = require( 'audio!VIBE/empty.mp3' );
   var Property = require( 'AXON/Property' );
   var vibe = require( 'VIBE/vibe' );
 
-  // Global property that allows all audio to be turned on/off, see #11
+  // sounds
+  var empty = require( 'audio!VIBE/empty.mp3' );
+
+  // global property that allows all audio to be turned on/off, see #11
   var audioEnabledProperty = new Property( true );
 
-  // Set up a single audio context that will be used by all sounds when
-  // using Web Audio API.
+  // Set up a single audio context that will be used by all sounds when using Web Audio API.
   var audioContext;
   if ( 'AudioContext' in window ) {
     audioContext = new AudioContext();
@@ -31,19 +31,16 @@ define( function( require ) {
   }
 
   /**
-   * @param {Array} soundInfoArray An array of 'soundInfo' objects.  Each
-   * soundInfo object includes *either* a url that points to the sound to be
-   * played *or* a base64-encoded version of the sound data.  The array is
-   * generally used to hold multiple formats for a given sound (e.g. mp3 and
-   * ogg).
+   * @param {Array} soundInfoArray An array of 'soundInfo' objects.  Each soundInfo object includes *either* a url that
+   * points to the sound to be played *or* a base64-encoded version of the sound data.  The array is generally used to
+   * hold multiple formats for a given sound (e.g. mp3 and ogg).
    * @constructor
    */
   function Sound( soundInfoArray ) {
 
     var self = this;
 
-    // For backward compatibility with earlier versions, support the case
-    // where a single soundInfo object is passed in.
+    // For backward compatibility with earlier versions, support the case where a single soundInfo object is passed in.
     var localSoundInfoArray = soundInfoArray;
     if ( !( soundInfoArray instanceof Array ) ) {
       localSoundInfoArray = new Array( soundInfoArray );
@@ -138,58 +135,61 @@ define( function( require ) {
 
   vibe.register( 'Sound', Sound );
 
-  /**
-   * Plays the sound using the Web Audio API if available or HTML5 audio if not.
-   * @public
-   */
-  Sound.prototype.play = function() {
-    if ( !Sound.audioEnabledProperty.get() ) {
-      return;
-    }
-    if ( audioContext ) {
-      // Use the Web Audio API.
-      if ( this.audioBuffer ) {  // This test is necessary to be sure that the audio has finished loading, see https://github.com/phetsims/vibe/issues/20
-        this.soundSource = audioContext.createBufferSource();
-        this.soundSource.buffer = this.audioBuffer;
-        this.soundSource.connect( audioContext.destination );
+  inherit( Object, Sound, {
 
-        if ( 'AudioContext' in window ) {
-          this.soundSource.start( 0 );
-        }
-        else if ( 'webkitAudioContext' in window ) {
-          this.soundSource.noteOn( 0 );
+    /**
+     * Plays the sound using the Web Audio API if available or HTML5 audio if not.
+     * @public
+     */
+    play: function() {
+      if ( !Sound.audioEnabledProperty.get() ) {
+        return;
+      }
+      if ( audioContext ) {
+        // Use the Web Audio API.  The following 'if' clause is necessary to be sure that the audio has finished
+        // loading, see https://github.com/phetsims/vibe/issues/20
+        if ( this.audioBuffer ) {
+          this.soundSource = audioContext.createBufferSource();
+          this.soundSource.buffer = this.audioBuffer;
+          this.soundSource.connect( audioContext.destination );
+
+          if ( 'AudioContext' in window ) {
+            this.soundSource.start( 0 );
+          }
+          else if ( 'webkitAudioContext' in window ) {
+            this.soundSource.noteOn( 0 );
+          }
         }
       }
-    }
-    else {
-      // Use the HTML5 API.
-      this.sound.play();
-    }
-  };
+      else {
+        // Use the HTML5 API.
+        this.sound.play();
+      }
+    },
 
-  /**
-   * Stop the sound if it is currently playing.
-   * @public
-   */
-  Sound.prototype.stop = function() {
-    if ( 'AudioContext' in window ) {
-      // TODO: TBD
+    stop: function() {
+      if ( 'AudioContext' in window ) {
+        // TODO: TBD
+      }
+      else if ( 'webkitAudioContext' in window ) {
+        // TODO: TBD
+      }
+      else {
+        this.sound.pause();
+        this.sound.currentTime = 0;
+      }
     }
-    else if ( 'webkitAudioContext' in window ) {
-      // TODO: TBD
-    }
-    else {
-      this.sound.pause();
-      this.sound.currentTime = 0;
-    }
-  };
+  }, {
 
-  // Workaround for iOS+Safari: In this situation, we must play an audio file
-  // from a thread initiated by a user event such as touchstart before any
-  // sounds will play. This is impossible with scenery, since all scenery
-  // events are batched and dispatched from the animation loop.
-  // See http://stackoverflow.com/questions/12517000/no-sound-on-ios-6-web-audio-api
-  // Note: This requires the user to touch the screen before audio can be played
+    // @public, @static, global control for audio on/off
+    audioEnabledProperty: audioEnabledProperty
+  } );
+
+  // Workaround for iOS+Safari: In this situation, we must play an audio file from a thread initiated by a user event
+  // such as touchstart before any sounds will play. This is not possible with scenery, since all scenery events are
+  // batched and dispatched from the animation loop. See
+  // http://stackoverflow.com/questions/12517000/no-sound-on-ios-6-web-audio-api Note: This requires the user to touch
+  // the screen before audio can be played
   if ( platform.mobileSafari ) {
 
     var silence = new Sound( empty );
@@ -200,8 +200,6 @@ define( function( require ) {
     window.addEventListener( 'touchstart', playSilence, false );
   }
 
-  // @pubic
-  Sound.audioEnabledProperty = audioEnabledProperty;
-
   return Sound;
+
 } );
