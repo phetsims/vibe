d48901f1471a1532ee6e9b5e723ebcdfdfdabd8f
samreid <reids@colorado.edu>
2020-02-27 12:08:03 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/vibe-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import vibe from './vibe.js';
+
+const vibeStrings = getStringModule( 'VIBE' );
+
+vibe.register( 'vibeStrings', vibeStrings );
+
+export default vibeStrings;
