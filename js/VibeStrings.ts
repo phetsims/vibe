37f8ddb1c4263d32b99c05c6e9885d834f7f8d20
Sam Reid <reids@colorado.edu>
2022-09-26 12:17:18 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/VibeStrings.ts
+++ js/VibeStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import vibe from './vibe.js';
 
 type StringsType = {
   'vibe': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
